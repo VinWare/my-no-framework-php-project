bce595158143009aa296fc15730c42a3dc1446f9
VinWare <VinWare@users.noreply.github.com>
2018-07-01 01:32:25 +0530

    Added error handling

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -6,4 +6,26 @@
  * Time: 01:01
  */
 declare(strict_types=1);
-echo 'Hello World!';
+
+namespace Example;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+//$environment = 'production';
+
+$whoops = new \Whoops\Run;
+if($environment !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function ($e){
+        // TODO: Friendly error page and send email to developer
+        echo 'TODO: Friendly error page and send email to developer';
+    });
+}
+
+$whoops->register();
+
+throw new \Exception;
