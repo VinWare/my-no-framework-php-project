813727f04db6b9fe3ebea3961303bc7e0065d3f3
VinWare <VinWare@users.noreply.github.com>
2018-07-01 01:09:01 +0530

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vineet
+ * Date: 01-07-2018
+ * Time: 00:56
+ */
+declare(strict_types=1);
+require __DIR__ . '/../src/Bootstrap.php';
---
+++ src/Bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vineet
+ * Date: 01-07-2018
+ * Time: 01:01
+ */
+declare(strict_types=1);
+echo 'Hello World!';
