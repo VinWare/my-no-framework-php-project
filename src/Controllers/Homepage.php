a950dafc9c33cf27be945d747f3cfd76b7094bd9
VinWare <VinWare@users.noreply.github.com>
2018-07-04 00:21:07 +0530

    Added Dependency Injection

--- src/Controllers/Homepage.php
+++ src/Controllers/Homepage.php
@@ -9,10 +9,19 @@ declare(strict_types=1);
 
 namespace Example\Controllers;
 
+use Http\Response;
 
 class Homepage
 {
-    public function show() {
-        echo 'Hello there World!';
+    private $response ;
+
+    public function __construct(Response $response)
+    {
+        $this->response = $response;
+    }
+
+    public function show()
+    {
+        $this->response->setContent('Hello World!');
     }
 }
