61dfc22f8af21145a331dedc1a5f736aa80007b7
VinWare <VinWare@users.noreply.github.com>
2018-07-03 17:46:02 +0530

    HTTP, Routing and Classes for pages

---
+++ src/Controllers/Homepage.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vineet
+ * Date: 03-07-2018
+ * Time: 17:40
+ */
+declare(strict_types=1);
+
+namespace Example\Controllers;
+
+
+class Homepage
+{
+    public function show() {
+        echo 'Hello there World!';
+    }
+}
---
+++ src/Routes.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vineet
+ * Date: 03-07-2018
+ * Time: 12:34
+ */
+declare(strict_types=1);
+
+return  [
+    ['GET', '/hello-world', ['Example\Controllers\Homepage', 'show']],
+    ['GET', '/another-route', function() {
+        echo 'This works too!';
+    }]
+];
